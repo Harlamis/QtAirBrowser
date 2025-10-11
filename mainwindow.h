e3b7e01bf104747be73ef694772bb2472338f268
Yeghor Danyliuk <egordan74@gmail.com>
2025-10-11 23:17:20 +0300

    add stylesheets to login screen; rename login_window to MainWindow to work properly

--- mainwindow.h
+++ mainwindow.h
@@ -5,7 +5,7 @@
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
-class Login_window;
+class MainWindow;
 }
 QT_END_NAMESPACE
 
@@ -18,6 +18,6 @@ public:
     ~MainWindow();
 
 private:
-    Ui::Login_window *ui;
+    Ui::MainWindow *ui;
 };
 #endif // MAINWINDOW_H
