6c62606440bcb00666557cb8860f65853da2a291
Yeghor <egordan74@gmail.com>
2025-11-13 15:47:54 +0200

    add fields to aircraft baseclass

--- AircraftProduct.h
+++ AircraftProduct.h
@@ -17,15 +17,21 @@ class AircraftProduct : public ISerializable {
     QString model_name;
     int model_number;
     double max_speed;
+    double price_per_unit;
+    int sold_units;
 public:
-    AircraftProduct(QString mname, int mnumber, double new_max_speed)
-        : model_name{mname}, model_number{mnumber}, max_speed{new_max_speed} {};
+    AircraftProduct(QString mname, int mnumber, double new_max_speed, double price, int sold)
+        : model_name{mname}, model_number{mnumber}, max_speed{new_max_speed}, price_per_unit{price}, sold_units{sold} {};
     AircraftProduct(QString mname)
-        : AircraftProduct(mname, 0,0) {};
+        : AircraftProduct(mname, 0,0,0,0) {};
     AircraftProduct(QString mname, int mnumber)
-        :AircraftProduct(mname,mnumber,0) {};
+        :AircraftProduct(mname,mnumber,0,0,0) {};
+    AircraftProduct(QString mname, int mnumber, double max_speed)
+        : AircraftProduct(mname,mnumber,max_speed,0,0) {};
+    AircraftProduct(QString mname, int mnumber, double max_speed, double price)
+        : AircraftProduct(mname,mnumber,max_speed,price,0) {};
     AircraftProduct()
-        : AircraftProduct("", 0,0) {};
+        : AircraftProduct("", 0,0,0,0) {};
     virtual ~AircraftProduct() {
         qDebug() << "Aircraft was destroyed\n";
     }
@@ -34,12 +40,16 @@ public:
         model_name = other.model_name;
         model_number = other.model_number;
         max_speed = other.max_speed;
+        price_per_unit = other.price_per_unit;
+        sold_units = other.sold_units;
     }
 
     AircraftProduct(AircraftProduct &&other) {
         model_name = std::move(other.model_name);
         model_number = std::move(other.model_number);
         max_speed = std::move(other.max_speed);
+        price_per_unit =std::move(other.price_per_unit);
+        sold_units = std::move(other.sold_units);
     }
     QString GetModelName() {
         return model_name;
@@ -51,6 +61,14 @@ public:
 
     double GetMax_speed() {
         return max_speed;
+    }
+
+    double GetPrice() {
+        return price_per_unit;
+    }
+
+    int GetSold_units() {
+        return sold_units;
     }
 
     void SetModelName(const QString &input) {
@@ -65,12 +83,23 @@ public:
         max_speed = input;
     }
 
+    void SetPrice(double &input) {
+        price_per_unit = input;
+    }
+
+    void SetSold_units(int &input) {
+        sold_units = input;
+    }
+
     virtual AircraftType GetType() = 0;
 
     virtual QJsonObject toJson() override {
         QJsonObject json;
         json["model_name"] = model_name;
         json["model_number"] = model_number;
+        json["max_speed"] = max_speed;
+        json["price_per_unit"] = price_per_unit;
+        json["sold_units"] = sold_units;
         return json;
     }
 };
