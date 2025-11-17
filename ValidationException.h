9a20515257b956242a6183c8934336de131f3a63
Yeghor Danyliuk <egordan74@gmail.com>
2025-11-17 18:30:58 +0200

    add exception class

---
+++ ValidationException.h
@@ -0,0 +1,22 @@
+#ifndef VALIDATIONEXCEPTION_H
+#define VALIDATIONEXCEPTION_H
+#include <exception>
+#include <QString>
+
+class ValidationExcpetion : public std::exception {
+    QString message;
+    QByteArray m_utf8Message;
+public:
+    explicit ValidationExcpetion(const QString& msg)
+        : message(msg) {
+        m_utf8Message = message.toUtf8();
+    };
+    QString getMessage() const {return message;};
+    const char* what() const noexcept override {
+        return m_utf8Message.constData();
+    }
+
+
+};
+
+#endif // VALIDATIONEXCEPTION_H
